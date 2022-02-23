e16ad6cead8c03536df9d9e9b49574a13fe2471f
SUNGJIN8481 <59166530+SUNGJIN8481@users.noreply.github.com>
2022-02-23 17:25:48 +0900

    Add files via upload

--- 1303.c
+++ 1303.c
@@ -7,58 +7,112 @@
 #define true 	1
 #define false	0
 
-#define _fail		-1
-#define _success	0
-
 #define MAX_SIZE 100
 char buff[MAX_SIZE+1][MAX_SIZE+1];
 int DFS_Visit[MAX_SIZE+1][MAX_SIZE+1];
+int N, M;
+int result;
 
 // 시작 행렬 체크하고 , 같은병사 다 찾을때까지 인접행렬 확인 후, 개수 확인.
 // 방문체크 안된 것들만 다시 DFS 진행.
 // DFS 진행 시, 시작행렬 체크 하고 시작.
 
-void DFS(int y, int x, char key)
+int DFS(int i, int j, char key)
 {
-	int i;
-	DFS_Visit[y][x] = true;
+	int cnt=0;
+	DFS_Visit[i][j] = true;
+//	printf("[%d][%d][%c]\n", i, j, key);
 
-	if(y > 0) //상
+	if(j > 0)	//좌
 	{
-		if(buff[y][x] == buff[y-1][x] == key && DFS_Visit[y][x] == false)	DFS(y-1, x, key);
+		if(buff[i][j] == buff[i][j-1] && buff[i][j-1] == key && DFS_Visit[i][j-1] == false)
+		{
+		//	printf("-------------- left\n");
+			cnt++;
+			result += DFS(i, j-1, key);
+		}
 	}
-	if(y < )
+	if(j < N)	//우
+	{
+		if(buff[i][j] == buff[i][j+1] && buff[i][j+1] == key && DFS_Visit[i][j+1] == false)
+		{
+		//	printf("-------------- right\n");
+			cnt++;
+			result += DFS(i, j+1, key);
+		}
+	}
+	if(i > 0)	//상
+	{
+		if(buff[i][j] == buff[i-1][j] && buff[i-1][j] == key && DFS_Visit[i-1][j] == false)
+		{
+		//	printf("-------------- up\n");
+			cnt++;
+			result += DFS(i-1, j, key);
+		}
+	}
+	if(i < M)	//하
+	{
+		if(buff[i][j] == buff[i+1][j] && buff[i+1][j] == key && DFS_Visit[i+1][j] == false)
+		{
+		//	printf("-------------- down\n");
+			cnt++;
+			result += DFS(i+1, j, key);
+		}
+	}
+//	printf("result : %d \n", result);
+	return cnt;
 }
 
 int main(void)
 {
-	int x, y, N, M;
-	int result_w, result_b;
+	int i, j;
+	int result_w=0, result_b=0;
+	int w=0, b=0;
 	char input='\0';
 
 	memset(buff, 0x00, sizeof(buff));
 	memset(DFS_Visit, 0x00, sizeof(DFS_Visit));
 
 	scanf("%d %d", &N, &M);
-	
-	for(y=0 ; y<M ; y++)
+
+	for(i=0 ; i<M ; i++)
 	{
-		for(x=0 ; x<N ; x++)
+		scanf("%s", buff[i]);
+	}
+/*
+	for(i=0 ; i<M ; i++)
+	{
+		for(j=0 ; j<N ; j++)
 		{
-			scanf(" %c", &input);
-			if(input == 'W')	buff[y][x] = 'W';
-			else				buff[y][x] = 'B';
+			printf("%c", buff[i][j]);
+		}
+		printf("\n");
+	}
+*/
+	for(i=0 ; i<M ; i++)
+	{
+		for(j=0 ; j<N ; j++)
+		{
+			if(DFS_Visit[i][j] == false)
+			{
+				if(buff[i][j] == 'W')
+				{
+					result = 1;
+					result_w = result + DFS(i, j, 'W');
+					w += (result_w*result_w);
+				//	printf("result_w: %d \n", result_w);
+				}
+				else
+				{
+					result = 1;
+					result_b = result + DFS(i, j, 'B');
+					b += (result_b*result_b);
+				//	printf("result_b: %d \n", result_b);
+				}
+			}
 		}
 	}
 
-	for(y=0 ; y<M ; y++)
-	{
-		for(x=0 ; x<N ; x++)
-		{
-			if(buff[y][x] == 'W')	DFS(y, x, 'W');
-			else					DFS(y, x, 'B');
-		}
-	}
-	
+	printf("%d %d\n", w, b);
 	return 0;
 }
