a995e2d8f789040990ad98452e186cde36e608f7
SUNGJIN8481 <59166530+SUNGJIN8481@users.noreply.github.com>
2022-03-10 08:21:58 +0900

    Add files via upload

---
+++ 16953.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+// 2를 곱한다.
+// 1을 수의 가장 오른쪽에 추가한다.
+// 1 <= A < B <= 10^9)
+
+int main(void)
+{
+	long A, B, cnt=1 ;
+
+	scanf("%d %d", &A, &B);
+
+	while(B > A)
+	{
+		if(B % 2 == 0)
+		{
+			B /= 2;
+			cnt++;
+		}
+		else
+		{
+			if(B % 10 == 1)
+			{
+				B--;
+				B /= 10;
+				cnt++;
+			}
+			else
+			{
+				printf("-1\n");
+				return 0;
+			}
+				
+		}
+	}
+
+	if(A == B)	printf("%lu\n", cnt);
+	else		printf("-1\n");
+	
+	return 0;
+}
